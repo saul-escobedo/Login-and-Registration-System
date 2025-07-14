68589a75cabf082089708022a50d5f412aca7c0c
Saul Escobedo <saulescobedoaraujo@gmail.com>
2025-07-14 13:55:26 -0500

    Fixed Registration Function

--- LoginSystem/Login and Registration System_NEW.cpp
+++ LoginSystem/Login and Registration System_NEW.cpp
@@ -1,95 +1,95 @@
-//FIXME: Try implementing  2D array for name and password assossiatied with the name.
-// Vector is better ; it can change in size.
-
+// Fix Login Option.
 #include <cstddef>
 #include <cstdio>
 #include <fstream>
 #include <ios>
 #include <iostream>
-#include <string>               // Use to_string & getline()
+#include <limits>
 #include <sstream>
+#include <string> // Use to_string & getline()
 
 void Display();
-void Register(std::string name, int password);
-void Login(std::string name, int password);
-void clearScreen() { std::cout << "\033[2J";}
+void Register(std::string name, std::string password);
+void Login(std::string name, std::string password);
+void clearScreen() { std::cout << "\033[2J"; }
 
 int main() {
 
-    Display();
+  Display();
 
-    return 0;
+  return 0;
 }
 
 void Display() {
-    int choice;
-    int password;
-    std::string name;
+  int choice;
+  std::string name, password;
 
-    std::cout << "1.  Login\n";
-    std::cout << "2.  Register\n";
-    std::cout << "Welcome, please enter your choice: ";
-    std::cin >> choice;
+  std::cout << "1.  Login\n";
+  std::cout << "2.  Register\n";
+  std::cout << "Welcome, please enter your choice: ";
+  std::cin >> choice;
 
-    switch (choice) {
-    case 1:
-        std::cout << "Enter your name: ";
-        std::cin >> name;
-        std::cout << "Enter your password: ";
-        std::cin >> password;
-        Login(name, password);
-        break;
-    case 2:
-        std::cout << "Enter your name: ";
-        std::cin >> name;
-        std::cout << "Enter your password: ";
-        std::cin >> password;
-        Register(name, password);
-        break;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    default:
-        std::cout << "That is not a valid choice." << std::endl;
-    }
+  switch (choice) {
+  case 1:
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    std::cout << "Enter your password: ";
+    std::getline(std::cin, password);
+    Login(name, password);
+    break;
+  case 2:
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    std::cout << "Enter your password: ";
+    std::getline(std::cin, password);
+    Register(name, password);
+    break;
+
+  default:
+    std::cout << "That is not a valid choice." << std::endl;
+  }
 }
 
-void Login(std::string name, int password) {
-  std::string file_Name;
-	int file_Password;
+void Login(std::string name, std::string password) {
+  std::string file_Name, file_Password;
 
-    std::ifstream my_file("RegistrationInfo.txt");
-    if (!my_file.is_open()) {
-        std::cout << "Error opening file.";
+  std::ifstream my_file("RegistrationInfo.txt");
+  if (!my_file.is_open()) {
+    std::cout << "Error opening file.";
+  }
+  std::string line;
+
+  while (std::getline(my_file, line)) {
+    std::istringstream iss(line);
+
+    std::string word;
+
+    iss >> file_Name >> file_Password;
+
+    size_t commaPos = line.find(',');
+    if (commaPos != std::string::npos) {
+      file_Name = line.substr(0, commaPos);
+      file_Password = line.substr(commaPos + 1);
     }
-    std::string line;
-
-    while (std::getline(my_file, line)) {
-      std::istringstream iss(line);
-
-      std::string word;
-
-      iss >> file_Name >> file_Password;
-      size_t commaPos = line.find(',');
-      if (commaPos != std::string::npos) {
-        file_Name = line.substr(0, commaPos);
-      }
-      if (name != file_Name || password != file_Password) {
-        std::cout << "Error. Incorrect name or password." << std::endl;
-      }
-      else {
-        clearScreen();
-        std::cout << "Welcome back " << name << "!" << std::endl;
-      }
+    if (name != file_Name || password != file_Password) {
+      std::cout << "Error. Incorrect name or password." << std::endl;
+    } else {
+      clearScreen();
+      std::cout << "Welcome back " << name << "!" << std::endl;
     }
-    my_file.close();
+  }
+  my_file.close();
 }
 
-void Register(std::string name, int password) {
-    std::ofstream my_file("RegistrationInfo.txt", std::ios::app);
-    if (!my_file.is_open()) {
-        std::cout << "Error opening file" << std::endl;
-    }
+void Register(std::string name, std::string password) {
+  std::ofstream my_file("RegistrationInfo.txt", std::ios::app);
+  if (!my_file.is_open()) {
+    std::cout << "Error opening file" << std::endl;
+  }
 
-    my_file << name << ", " << password << std::endl;
+  my_file << name << ", " << password << std::endl;
 
-    my_file.close();
+  my_file.close();
 }

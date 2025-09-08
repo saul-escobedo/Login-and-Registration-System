d5ea737e0df6a6650f8979070656109308ed2728
Saul Escobedo <saulescobedoaraujo@gmail.com>
2025-09-08 13:15:07 -0500

    Need Finishing Touches

--- School/Project 1/House.cpp
+++ School/Project 1/House.cpp
@@ -19,7 +19,7 @@ class HouseInfo {
     HouseInfo( string style = "", int Num_Of_Bedrooms = 0, int Num_Of_Bathrooms = 0, int Num_Of_Cars_Garage = 0, int Year_Built= 0, int Finished_Square_Footage = 0, double Price = 0.0, double Tax = 0.0);
     //setters and getrer
     void print() {
-      cout << left << setw(10) << style << Num_Of_Bedrooms << Num_Of_Bathrooms << Num_Of_Cars_Garage << Year_Built << Finished_Square_Footage << Price << Tax << endl;
+      cout << left << setw(15) << style << setw(10) << Num_Of_Bedrooms << setw(10) << Num_Of_Bathrooms << setw(10) << Num_Of_Cars_Garage << setw(10) << Year_Built << setw(10) << Finished_Square_Footage << setw(10) << Price << setw(10) << Tax << endl;
     }
     void ReadFile();
     void static loadHouse(HouseInfo houses[], int arrayZise, string file);
@@ -100,6 +100,7 @@ void HouseInfo::loadHouse(HouseInfo *houses, int arrayZise, string file) {
     houses[i].Finished_Square_Footage = var6;
     houses[i].Price = var7;
     houses[i].Tax = var8;
+    houses[i].print();
 
     i++;
 
@@ -109,8 +110,9 @@ void HouseInfo::loadHouse(HouseInfo *houses, int arrayZise, string file) {
 
 int main() {
   HouseInfo Houses[7];
+
+  cout << left << setw(15) << "House Style" << setw(10) << "Bed Rms" << setw(10) << "Bath Rms" << setw(10) << "Garage"<< setw(10) << "Year" << setw(10) << "Area" << setw(10) << "Price" << setw(10) << "Tax Paid" << endl;
   HouseInfo::loadHouse(Houses, 7, "HouseInfo.txt");
-  Houses[1].print();
 
   return 0;
 

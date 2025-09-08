942b23b665fb4ac56f3cb57308d307e4d406218f
Saul Escobedo <saulescobedoaraujo@gmail.com>
2025-09-08 11:32:01 -0500

    Finish print() function.

--- School/Project 1/House.cpp
+++ School/Project 1/House.cpp
@@ -1,5 +1,4 @@
-//TODO: Make setters and Getters
-
+#include <ios>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -23,6 +22,7 @@ class HouseInfo {
       cout << left << setw(10) << style << Num_Of_Bedrooms << Num_Of_Bathrooms << Num_Of_Cars_Garage << Year_Built << Finished_Square_Footage << Price << Tax << endl;
     }
     void ReadFile();
+    void static loadHouse(HouseInfo houses[], int arrayZise, string file);
 
     //Setters
     void setStyle(string sty) {style = sty;}
@@ -34,7 +34,14 @@ class HouseInfo {
     void setPrice(double price) {Price = price;}
     void setTax(double tax) {Tax = tax;}
     //Getters
-    v
+    string getStyle() {return style;}
+    int getBedrooms() {return Num_Of_Bedrooms;}
+    int getBathrooms() {return Num_Of_Bathrooms;}
+    int getCars() {return Num_Of_Cars_Garage;}
+    int getYear() {return Year_Built;}
+    int getFootage() {return Finished_Square_Footage;}
+    double getPrice() {return Price;}
+    double getTax() {return Tax;}
 };
 
 HouseInfo::HouseInfo(string style, int Num_Of_Bedrooms, int Num_Of_Bathrooms, int Num_Of_Cars_Garage, int Year_Built, int Finished_Square_Footage, double Price, double Tax)
@@ -68,10 +75,42 @@ void HouseInfo::ReadFile() {
 
 }
 
+void HouseInfo::loadHouse(HouseInfo *houses, int arrayZise, string file) {
+  string line;
+
+  ifstream MyFile(file, ios::out);
+  if(!MyFile.is_open()) {
+    cout << "<><> Error Opening File <><> \n";
+  }
+
+  int i =0;
+  while(getline(MyFile, line)) {
+    istringstream iss(line);
+    string var1;
+    int var2, var3, var4, var5, var6;
+    double var7, var8;
+
+    iss >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8;
+
+    houses[i].style = var1;
+    houses[i].Num_Of_Bedrooms = var2;
+    houses[i].Num_Of_Bathrooms = var3;
+    houses[i].Num_Of_Cars_Garage = var4;
+    houses[i].Year_Built = var5;
+    houses[i].Finished_Square_Footage = var6;
+    houses[i].Price = var7;
+    houses[i].Tax = var8;
+
+    i++;
+
+    if(arrayZise == i) {break;}
+  }
+}
 
 int main() {
-  HouseInfo fv;
-  fv.print();
+  HouseInfo Houses[7];
+  HouseInfo::loadHouse(Houses, 7, "HouseInfo.txt");
+  Houses[1].print();
 
   return 0;
 
